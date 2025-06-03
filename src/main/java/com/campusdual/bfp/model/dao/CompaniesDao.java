9775d4219ed60f6153ae3c0b58f3ad7e8c0e7345
Nuria Calo <nuriacalomosquera@gmail.com>
2025-06-03 14:08:25 +0200

    Revert "Added companies, companiesDao, companiesDTO, companiesService, CompaniesMapper and companiesController"
    This reverts commit afe6d57615ebbd172b368bbaeef84e1396dcebd3.

--- src/main/java/com/campusdual/bfp/model/dao/CompaniesDao.java
+++
@@ -1,7 +0,0 @@
-package com.campusdual.bfp.model.dao;
-
-import com.campusdual.bfp.model.Companies;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompaniesDao extends JpaRepository<Companies, Integer> {
-}
