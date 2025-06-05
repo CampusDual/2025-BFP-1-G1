00e9ca38050160f8633d479963e0eba7e4751444
AlexBDominguez <alexwirtzfp@gmail.com>
2025-06-05 12:58:53 +0200

    Añadido componente login-box y configuraciones

--- Front/jobPortal/src/app/app.module.ts
+++ Front/jobPortal/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginBoxComponent } from './login-box/login-box.component';
+import { ReactiveFormsModule } from '@angular/forms'; 
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front/jobPortal/src/app/login-box/login-box.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-box',
+  templateUrl: './login-box.component.html',
+  styleUrls: ['./login-box.component.css']
+})
+export class LoginBoxComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }else{
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
