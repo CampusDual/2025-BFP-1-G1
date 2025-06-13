3330ebccd4c990280eb3027e2b38b615788d38fa
AlexBDominguez <alexwirtzfp@gmail.com>
2025-06-13 08:48:54 +0200

    Añadidos cambios para el catálogo de trabajos, incluyendo nuevos componentes y servicios

---
+++ Front/jobPortal/src/app/main/job-catalogue/job-catalogue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-catalogue',
+  templateUrl: './job-catalogue.component.html',
+  styleUrls: ['./job-catalogue.component.css']
+})
+export class JobCatalogueComponent {
+
+}
--- Front/jobPortal/src/app/main/main-routing.module.ts
+++ Front/jobPortal/src/app/main/main-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { LoginBoxComponent } from './login-box/login-box.component';
+import { JobCatalogueComponent } from './job-catalogue/job-catalogue.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginBoxComponent },
   { path: 'userprofile', component: UserProfileComponent },
+  { path: 'catalogue', component: JobCatalogueComponent },
+  
 ];
 
 @NgModule({
