4675fbe61391598ff80480fd88cc187c88390dde
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-06-18 13:51:39 +0200

    change login style

--- Front/jobPortal/src/app/app.component.ts
+++ Front/jobPortal/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'jobPortal';
+  isLogin = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isLogin = this.router.url === '/main/login';
+      }
+    });
+  }
 }
