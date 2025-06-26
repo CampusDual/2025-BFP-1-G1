fe4b651577ddfe8cd8bf4d4d17207818196d2534
SilviaGarciaBouza <silvia.gar.bou.dev@gmail.com>
2025-06-26 12:06:17 +0200

    fixed css search

--- Front/jobPortal/src/app/header/header.component.ts
+++ Front/jobPortal/src/app/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../services/users.service';
-
+import { User } from '../model/user';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -9,13 +9,20 @@ import { UsersService } from '../services/users.service';
 })
 export class HeaderComponent implements OnInit {
   user: User | null = null;
-
   constructor(public usersService: UsersService, private router: Router) {}
-
+  ngOnInit(): void {
+    // Suscríbete al observable user$
+    this.usersService.user$.subscribe((user) => {
+      this.user = user;
+    });
+    // Si no hay usuario cargado pero hay token, pide el perfil
+    if (!this.user && this.usersService.isLoggedIn()) {
+      this.usersService.getUserProfile().subscribe();
+    }
+  }
   goToCatalogue(): void {
     this.router.navigate(['/main/catalogue']);
   }
-
   redirectToProfile(): void {
     if (this.usersService.isLoggedIn()) {
       this.router.navigate(['/main/userprofile']);
@@ -26,11 +33,9 @@ export class HeaderComponent implements OnInit {
   goToLogin(): void {
     this.router.navigate(['/main/login']);
   }
-
   //  goToRegister(): void {
   //  this.router.navigate(['/main/register']);
   //  }
-
   logout(): void {
     this.usersService.logout();
     this.router.navigate(['/main/login']);
