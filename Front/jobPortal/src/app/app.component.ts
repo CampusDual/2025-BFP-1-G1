150eb2a2ab03d60ed8302ed5cc296d1348475b79
AngelPG77 <angelpugagomez@gmail.com>
2025-07-15 15:00:59 +0200

    Added css styles for Candidate-details

--- Front/jobPortal/src/app/app.component.ts
+++ Front/jobPortal/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
@@ -10,11 +12,19 @@ export class AppComponent {
   title = 'jobPortal';
   isLogin = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.isLogin = this.router.url === '/main/login' || this.router.url === '/main/signup';
       }
     });
+    this.iconRegistry.addSvgIcon(
+      'linkedin',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/linkedin.svg')
+    );
+    this.iconRegistry.addSvgIcon(
+      'github',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icon/github.svg')
+    );
   }
 }
--- Front/jobPortal/src/app/main/candidate-profile/candidate-details/candidate-details.component.ts
+++ Front/jobPortal/src/app/main/candidate-profile/candidate-details/candidate-details.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidate } from 'src/app/model/candidate';
 import { Education } from 'src/app/model/education';
 import { UserData } from 'src/app/model/userData';
 
 import { WorkExperience } from 'src/app/model/workExperience';
+import { CandidateProfileService } from 'src/app/services/candidate-profile.service';
 import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-candidate-details',
   templateUrl: './candidate-details.component.html',
   styleUrls: ['./candidate-details.component.css'],
+
 })
+
 export class CandidateDetailsComponent implements OnInit {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private candidateService: CandidateProfileService
+  ) {}
   userData: UserData | null = null;
-
-
-  ngOnInit(): void {
-    this.userService.userData$.subscribe((user) => {
-      this.userData = user;
-      console.log('User data loaded:', this.userData);
-    });
-  }
+  candidate!: Candidate;
 
   workExperience: WorkExperience = {
     idCandidate: this.userData?.candidate?.id ?? 0,
@@ -39,6 +39,17 @@ export class CandidateDetailsComponent implements OnInit {
     endPeriod: '',
     description: '',
   };
+
+  ngOnInit(): void {
+    this.userService.userData$.subscribe((user) => {
+      this.userData = user;
+      console.log('User data loaded:', this.userData);
+      this.candidateService.getCandidateProfile().subscribe((candidate) => {
+        this.candidate = candidate;
+        console.log('Candidate profile loaded:', this.candidate);
+      });
+    });
+  }
 
   experiences: WorkExperience[] = [];
   educations: Education[] = [];
@@ -65,5 +76,25 @@ export class CandidateDetailsComponent implements OnInit {
       endPeriod: '',
       description: '',
     };
+  }
+
+  isCandidate(): boolean {
+    return (
+      !!this.userData?.candidate ||
+      (!!this.userData?.user && this.userData.user.role_id === 3)
+    );
+  }
+
+  isOwner(): boolean {
+    if (!this.userData?.candidate?.id || !this.candidate?.id) {
+      return false;
+    }
+    return this.userData.candidate.id === this.candidate.id;
+  }
+  editCandidate(): void {}
+
+  capitalizeFirst(text: string | undefined | null): string {
+    if (!text) return '';
+    return text.charAt(0).toUpperCase() + text.slice(1);
   }
 }
