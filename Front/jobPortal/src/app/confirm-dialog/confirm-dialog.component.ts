c255f57bd67cfa2be5bb14ce31cdc7ab52522644
Nuria Calo <nuriacalomosquera@gmail.com>
2025-07-18 08:40:50 +0200

    Update confirm dialog and admin profile

--- Front/jobPortal/src/app/confirm-dialog/confirm-dialog.component.ts
+++ Front/jobPortal/src/app/confirm-dialog/confirm-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ConfirmDialogComponent {
   constructor(
